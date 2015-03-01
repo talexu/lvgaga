fc638022f6ecf2e3bf4fbb35eba21a033f1ad4b9
Tale Xu <bjutales@hotmail.com>
2015-03-01 22:38:37 +0900

    Add comment page

---
+++ Lvgaga/Lvgaga/Controllers/CommentController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lvgaga.Controllers
+{
+    [RoutePrefix("comments")]
+    public class CommentController : Controller
+    {
+        // GET: comment
+        [Route]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{id}")]
+        // GET: comments/5
+        public ActionResult Show(int id)
+        {
+            return View();
+        }
+    }
+}
