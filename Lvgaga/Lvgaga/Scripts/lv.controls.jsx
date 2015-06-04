b153c8fb6a75497ed69fce5407469c9bd104e3d4
Xu, Yunlong | Long | SDTD <yunlong.xu@mail.rakuten.com>
2015-06-04 16:32:30 +0900

    add initial data binding

--- Lvgaga/Lvgaga/Scripts/lv.controls.jsx
+++ Lvgaga/Lvgaga/Scripts/lv.controls.jsx
@@ -1,69 +1,94 @@
 ﻿var Tumblr = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <p>Hello</p>
-      </div>
-    );
-  }
+    render: function () {
+        var ctx = this.props.dataContext;
+        return (
+            <div>
+                <img src={ctx.MediaLargeUri}></img>
+
+                <div>
+                    <p>{ctx.Text}</p>
+
+                    <p>{ctx.CreateTime}</p>
+                </div>
+            </div>
+        );
+    }
 });
 
 var Functions = React.createClass({
-  render: function() {
-    return (
-      <div>
-        Hello, world! I am a Functions.
-      </div>
-    );
-  }
+    render: function () {
+        return (
+            <div>
+                Hello, world! I am a Functions.
+            </div>
+        );
+    }
 });
 
 var CommentForm = React.createClass({
-  render: function() {
-    return (
-      <div>
-        Hello, world! I am a CommentForm.
-      </div>
-    );
-  }
+    render: function () {
+        return (
+            <div>
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
 });
 
 var Comment = React.createClass({
-  render: function() {
-    return (
-      <div>
-        Hello, world! I am a Comment.
-      </div>
-    );
-  }
+    render: function () {
+        var ctx = this.props.dataContext;
+        return (
+            <div>
+                <p>{ctx.UserName}</p>
+
+                <p>{ctx.CommentTime}</p>
+
+                <p>{ctx.Text}</p>
+            </div>
+        );
+    }
 });
 
 var CommentList = React.createClass({
-  render: function() {
-    return (
-      <div>
-        Hello, world! I am a CommentList.
-      </div>
-    );
-  }
+    render: function () {
+        var commentNodes = this.props.dataContext.map(function (comment) {
+            return (
+                <Comment dataContext={comment}/>
+            );
+        });
+        return (
+            <div>
+                {commentNodes}
+            </div>
+        );
+    }
 });
 
 var TumblrBox = React.createClass({
-  render: function() {
-    return (
-      <div>
-        Hello, world! I am a TumblrBox.
-      </div>
-    );
-  }
+    render: function () {
+        return (
+            <div>
+                <Tumblr dataContext={this.props.dataContext}/>
+                <Functions />
+                <CommentForm />
+                <CommentList dataContext={this.props.dataContext.comments}/>
+            </div>
+        );
+    }
 });
 
 var TumblrBoxList = React.createClass({
-  render: function() {
-    return (
-      <div>
-        Hello, world! I am a TumblrBoxList.
-      </div>
-    );
-  }
+    render: function () {
+        var tumblrBoxNodes = this.props.dataContext.map(function (tumblr) {
+            return (
+                <TumblrBox dataContext={tumblr}/>
+            );
+        });
+        return (
+            <div>
+                {tumblrBoxNodes}
+            </div>
+        );
+    }
 });
--- Lvgaga/Lvgaga/Scripts/lv.tumblr.jsx
+++ Lvgaga/Lvgaga/Scripts/lv.tumblr.jsx
@@ -1,4 +1,45 @@
-﻿React.render(
-  <Tumblr />,
+﻿var tumblrModels = [
+  {
+    MediaLargeUri: "https://qingyulustg.blob.core.windows.net/large/3b40a530-1b85-423e-b65c-6f14cf114d22.jpg",
+    Text: "你也曾当过笨蛋，也曾试着当瞎子当聋子的去信任一个人，你也知道世界上最可悲的就是自我欺骗，但是人笨过傻过瞎过就够了，你更要懂得爱自己，而不是一直重蹈覆辙，还自以为多痴情。",
+    CreateTime: "2015-06-02 16:33:41",
+    tp: "",
+    rk: "",
+    comments: [
+      {
+        UserName: "bjutales@hotmail.com",
+        CommentTime: "2015-06-02 16:33:41",
+        Text: "一些评论"
+      },
+      {
+        UserName: "bjutales@hotmail.com",
+        CommentTime: "2015-06-02 16:33:41",
+        Text: "一些评论"
+      }
+    ]
+  },
+  {
+    MediaLargeUri: "https://qingyulustg.blob.core.windows.net/large/3b40a530-1b85-423e-b65c-6f14cf114d22.jpg",
+    Text: "你也曾当过笨蛋，也曾试着当瞎子当聋子的去信任一个人，你也知道世界上最可悲的就是自我欺骗，但是人笨过傻过瞎过就够了，你更要懂得爱自己，而不是一直重蹈覆辙，还自以为多痴情。",
+    CreateTime: "2015-06-02 16:33:41",
+    tp: "",
+    rk: "",
+    comments: [
+      {
+        UserName: "bjutales@hotmail.com",
+        CommentTime: "2015-06-02 16:33:41",
+        Text: "一些评论"
+      },
+      {
+        UserName: "bjutales@hotmail.com",
+        CommentTime: "2015-06-02 16:33:41",
+        Text: "一些评论"
+      }
+    ]
+  }
+];
+
+React.render(
+  <TumblrBoxList dataContext={tumblrModels} />,
   document.getElementById('div_tumblrs')
 );
