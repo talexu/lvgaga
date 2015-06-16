c627b48201e6f556b346ff4ea510476199e06849
Tale Xu <bjutales@hotmail.com>
2015-06-16 20:28:03 +0900

    execlude ipad in mobile

--- Lvgaga/Lvgaga/Global.asax.cs
+++ Lvgaga/Lvgaga/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace Lvgaga
 {
@@ -10,11 +12,18 @@ namespace Lvgaga
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();     
+            AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode
+            {
+                ContextCondition = (context => context.Request.UserAgent != null &&
+                                               context.GetOverriddenUserAgent()
+                                                   .IndexOf("iPad", StringComparison.OrdinalIgnoreCase) >= 0)
+            });
         }
     }
 }
