414f3cdb254f903090ceef05424882372d6071e0
Tale Xu <bjutales@hotmail.com>
2015-06-24 00:06:44 +0900

    remove unused js files

--- Lvgaga/Lvgaga/Scripts/lv.token.js
+++
@@ -1,9 +0,0 @@
-﻿(function () {
-    var getToken = function (paths) {
-        return $.get(sprintf("/api/v1/tokens/%s", paths.join("/"))).retry({
-            times: lv.defaultRetryTime
-        });
-    };
-
-    lv.token.getToken = getToken;
-})();
